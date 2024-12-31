66d39f4048142126a5f2dcd55d38eca669187b52
Kondi Kalèd ABODJI <kaledabodji@gmail.com>
2024-12-31 01:15:12 +0100

    Adding Template_level_1 Home page.

---
+++ Elearning_Test/Controllers/MyHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elearning_Test.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        public IActionResult homePage()
+        {
+            return View();
+        }
+    }
+}
