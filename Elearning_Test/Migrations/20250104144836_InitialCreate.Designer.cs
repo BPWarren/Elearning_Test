62d1c1cb32e40e32391eaf61869109a24dec3fbf
Kondi Kalèd ABODJI <kaledabodji@gmail.com>
2025-01-04 20:12:01 +0100

    migrations

--- Elearning_Test/Migrations/20250104144836_InitialCreate.Designer.cs
+++ Elearning_Test/Migrations/20250104144836_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Elearning_Test.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250103011534_CorrectPriceToDecimal")]
-    partial class CorrectPriceToDecimal
+    [Migration("20250104144836_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
