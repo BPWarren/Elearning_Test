fe9fb76fe7cc0bd61a64fba55bfcf7e6ee1fe555
Espoir <dskd2805@gmail.com>
2025-01-07 01:44:05 +0100

    toujours le dashboard admin

--- Elearning_Test/Models/DashboardViewModel.cs
+++ Elearning_Test/Models/DashboardViewModel.cs
@@ -2,52 +2,113 @@
 {
     public class DashboardViewModel
     {
+        // Statistiques principales
         public int TotalEtudiants
         {
             get; set;
-        }
+        } // Nombre total d'étudiants
         public int TotalProfesseurs
         {
             get; set;
-        }
+        } // Nombre total de professeurs
+        public int TotalCertificatsValides
+        {
+            get; set;
+        } // Nombre de certificats validés
+        public int TotalCertificatsEnAttente
+        {
+            get; set;
+        } // Nombre de certificats en attente
         public int TotalCours
         {
             get; set;
-        }
-        public int TotalCertificats
+        } // Nombre total de cours
+        public int TotalInscriptions
         {
             get; set;
-        }
-        public int CoursActifs
-        {
-            get; set;
-        }
-        public int CoursTermines
-        {
-            get; set;
-        }
-        public int EtudiantsActifs
-        {
-            get; set;
-        }
-        public int ProfesseursActifs
-        {
-            get; set;
-        }
+        } // Nombre total d'inscriptions
 
-        public int NombreVisiteur
+        // Statistiques d'activité
+        public int ParticipantsActifs
         {
             get; set;
-        }
-
-        // Pour les graphiques
+        } // Nombre d'étudiants actifs (connectés récemment)
+        public int FormateursActifs
+        {
+            get; set;
+        } 
+        //}
+        // Graphiques
         public required Dictionary<string, int> InscriptionsParMois
         {
             get; set;
-        } // Mois => Nombre d'inscriptions
+        }
+
+        public required List<Certification> CertifValide
+
+        {
+            get; set;
+        }
+
+        public required List<Certification> CertifEnAttente
+
+        {
+            get; set;
+        }
+
+        public required List<Etudiant> NouveauxEtudiants
+        {
+            get; set;
+        } // Liste des 5 nouveaux étudiants inscrits
+        public required List<Cours> NouveauxCours
+        {
+            get; set;
+        } // Liste des 5 nouveaux cours ajoutés
+
+        // Autres éléments essentiels
+        public int TotalVisiteurs
+        {
+            get; set;
+        } // Nombre total de visiteurs
+        public int TotalPaiements
+        {
+            get; set;
+        } // Nombre total de paiements effectués
+        public decimal RevenuTotal
+        {
+            get; set;
+        } // Revenu total généré par les cours
+
         public required Dictionary<string, int> CoursPopulaires
         {
             get; set;
-        } // Nom du cours => Nombre d'étudiants
+        } // Nom du cours => Nombre d'étudiants inscrits
+
+        // Utilisateurs en ligne
+        public required List<UserInfo> EtudiantsEnLigne
+        {
+            get; set;
+        } // Liste des étudiants actuellement en ligne
+        public required List<UserInfo> ProfesseursEnLigne
+        {
+            get; set;
+        }
+    }
+
+    public class UserInfo
+    {
+        public required string Nom
+        {
+            get; set;
+        }
+
+        public required string Prenom
+        {
+            get; set;
+        }
+        public required string Email
+        {
+            get; set;
+        }
     }
 }
